d98d268be35c89ad1f9d8eeb852cd8250d8db202
Mess <mess@winningponies.com>
2021-03-04 20:29:13 -0700

    Added hero images to blog posts.

--- src/pages/blog/{mdx.slug}.js
+++ src/pages/blog/{mdx.slug}.js
@@ -2,12 +2,15 @@ import * as React from "react"
 import Layout from "../../components/layout"
 import {graphql} from "gatsby"
 import {MDXRenderer} from "gatsby-plugin-mdx"
+import {GatsbyImage, getImage} from "gatsby-plugin-image"
 
 const BlogPage = ({data}) => {
+  const image = getImage(data.mdx.frontmatter.hero_image)
   return (
     <Layout>
       <h1>{data.mdx.frontmatter.title}</h1>
-      <p>{data.mdx.frontmatter.date}</p>
+      <p>Posted: {data.mdx.frontmatter.date}</p>
+      <GatsbyImage image={image} alt="" />
       <MDXRenderer>{data.mdx.body}</MDXRenderer>
     </Layout>
   )
@@ -19,6 +22,15 @@ export const query = graphql`
       frontmatter {
         date(formatString: "MMMM DD YYYY")
         title
+        hero_image {
+          childImageSharp {
+            gatsbyImageData(
+              formats: [JPG,WEBP,AVIF]
+              placeholder: TRACED_SVG
+              avifOptions: {speed: 10}
+            )
+          }
+        }
       }
       body
     }
