e0cdddcf1578392997da69638f43010b9a020f50
Mess <mess@winningponies.com>
2021-03-04 19:41:18 -0700

    Flesh out blog page with MDXRenderer.

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,22 +1,35 @@
 import * as React from "react"
 import Layout from "../components/layout"
 import {graphql} from "gatsby"
+import {MDXRenderer} from "gatsby-plugin-mdx"
 
 const BlogPage = ({data}) => {
   return (
     <Layout>
       <ul>
-        {data.allFile.nodes.map((el) => <li>{el.name}</li>)}
+        {data.allMdx.nodes.map((node) =>
+          <li>
+            <h2>{node.frontmatter.title}</h2>
+            <p>Posted: {node.frontmatter.date}</p>
+            <p>
+              <MDXRenderer>{node.body}</MDXRenderer>
+            </p>
+          </li>
+        )}
       </ul>
     </Layout>
   )
 }
 
 export const query = graphql`
-  query BlogQuery {
-    allFile(filter: {sourceInstanceName: {eq: "blog"}}) {
+  query MyBlogQuery {
+    allMdx {
       nodes {
-        name
+        body
+        frontmatter {
+          date(formatString: "MMMM D, YYYY")
+          title
+        }
       }
     }
   }
